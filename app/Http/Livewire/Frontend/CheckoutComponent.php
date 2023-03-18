c9a7724d6c86e8fcf499017831525680e073ccaa
medAmezian <amezian766@gmail.com>
2023-03-18 03:08:25 +0100

    new edit

--- app/Http/Livewire/Frontend/CheckoutComponent.php
+++ app/Http/Livewire/Frontend/CheckoutComponent.php
@@ -4,10 +4,13 @@ namespace App\Http\Livewire\Frontend;
 
 use Livewire\Component;
 
+use App\Models\Category;
+
 class CheckoutComponent extends Component
 {
     public function render()
-    {
-        return view('livewire.frontend.checkout-component');
+    {        
+        $categories = Category::all();
+        return view('livewire.frontend.checkout-component')->layout('layouts.app',compact("categories"));
     }
 }
